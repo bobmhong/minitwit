a27f6d4d20b07799a366cb4fb58e96a9d000caf2
Bob Hong <bobmhong@gmail.com>
2016-09-20 14:21:57 -0400

    updated password

--- ci/test-integration.sh
+++ ci/test-integration.sh
@@ -15,8 +15,8 @@ COOKIE_JAR="cookies-$(date | md5sum | head -c 10).txt"
 
 cat > minitwit.env << EOF
 SPRING_DATASOURCE_URL=jdbc:mysql://mysql.marathon.mesos:3306/minitwit?autoReconnect=true&useSSL=false
-SPRING_DATASOURCE_USERNAME=minitwit
-SPRING_DATASOURCE_PASSWORD=minitwit
+SPRING_DATASOURCE_USERNAME=minitwit3
+SPRING_DATASOURCE_PASSWORD=minitwit3
 SPRING_DATASOURCE_DRIVER-CLASS-NAME=com.mysql.cj.jdbc.Driver
 SPRING_DATASOURCE_PLATFORM=mysql
 EOF
